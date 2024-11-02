bd5547e91b2d7b0d043f6fa4cadfaf786239f9bc
itsmeeSanj <itsmeesanj@gmail.com>
2024-11-02 16:36:23 -0400

    add:readmore-readless

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
+import TextExpander from "./TextExpander";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating
+    {/* <StarRating
       maxRating={5}
       defaultSize={20}
       messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
+      defaultRating={3} */}
+    <TextExpander />
   </React.StrictMode>
 );
